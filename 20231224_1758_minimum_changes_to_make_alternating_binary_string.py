b4b979151238a7b859c9d9523b3923532de0f80d
Raphael A. P. Oliveira <roliveira.astro@gmail.com>
2023-12-24 12:37:59 +0100

    Added solution of 24.dec, alternating binary strings

---
+++ 20231224_1758_minimum_changes_to_make_alternating_binary_string.py
@@ -0,0 +1,16 @@
+# (c) Raphael A. P. Oliveira, astronomer
+# My solution for the problem 1758, given in this link:
+# https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
+
+# This is my 1st solution (beats 66% in runtime and 6% in memory)
+class Solution:
+    def minOperations(self, s: str) -> int:
+        length = len(s)
+        opt_1 = "01" * ((length+1) // 2)
+        opt_2 = "10" * ((length+1) // 2)
+        opt_1, opt_2 = opt_1[:length], opt_2[:length]
+
+        diff_1 = sum(s[i] != opt_1[i] for i in range(length))
+        diff_2 = sum(s[i] != opt_2[i] for i in range(length))
+
+        return min(diff_1, diff_2)
